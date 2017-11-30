fe749b6d5a90725c941b53b2bcc73e580a41b462
zhun0606 <33014324+zhun0606@users.noreply.github.com>
2017-11-30 17:41:50 +0800

    Sprint 2 Submission

---
+++ src/Employee.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment;
+
+/**
+ *
+ * @author taruc
+ */
+public class Employee {
+    
+    // Data Member
+    private String restaurantName;
+    String restaturantName[];
+    
+    
+    public void setRestaurantName(String restaurantName)
+    {
+        this.restaurantName = restaurantName;
+    }
+    
+    
+    
+    
+    public String getRestaurantName()
+    {
+        return restaurantName;
+    }
+}
---
+++ src/Restaurant.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment;
+
+/**
+ *
+ * @author user
+ */
+public class Restaurant {
+    
+    // Data Type
+    private String restaurantName;
+    
+    public void setRestaurantName(String restaurantName)
+    {
+        this.restaurantName = restaurantName;
+    }
+    
+    public String getRestaurantName()
+    {
+        return restaurantName;
+    }
+    
+}
